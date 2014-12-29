c7b16d89e4d370c89da8bca4d6ecea48b112aafb
Carlos J <karlutxo@gmail.com>
2014-12-29 19:33:47 +0000

    test commit2

--- app/src/androidTest/java/com/karlutxo/test1/ApplicationTest.java
+++ app/src/androidTest/java/com/karlutxo/test1/ApplicationTest.java
@@ -7,7 +7,7 @@ import android.test.ApplicationTestCase;
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
 
-// comentario de prueba
+// comentario de prueba1
 public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
         super(Application.class);
